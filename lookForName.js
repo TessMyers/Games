3af86e764c3ae2a838f76d9b45cfbffb1632e98d
Tess Myers <tess@tessmyers.com>
2014-02-13 00:14:05 -0500

    simple games based on improved codeacademy projects

---
+++ lookForName.js
@@ -0,0 +1,27 @@
+//based off of codeacademy exercise, Paul helped me improve this. 
+
+var text = "why hello there tess, I hear that I need to plant a name in this box a few times, tess. what do you think of that. I wonder if punctuation will effect this at all, hmm, tess?"
+var myName = "tess"
+var hits = []
+for(var i = 0; i < text.length; i++) {
+    if (text[i] === "t") {
+      var preHits = []
+        for (var j = i; j < (myName.length + i); j++) {
+            preHits.push(text[j])
+        }
+
+
+      if (preHits.join("") === myName) {
+        hits.push(preHits.join(""))
+      }
+    }
+}
+if (hits.length === 0) {
+    console.log("Your name wasn't found!")
+} else {
+    console.log(hits)
+}
+
+
+//["tess", "tess", "tess"]
+// fuckyeah
---
+++ ohNoMayo.js
@@ -0,0 +1,30 @@
+<script>
+var condiment = prompt("Is it mayo? Enter questionable condiment to find out:"); 
+
+if (condiment === "mayo") {
+	alert("Oh fuck, it's mayo! RUN FOR IT.");
+else if(condiment === "aioli") {
+	alert("Aioli is nectar of the gods and has absolutely nothing whatsoever in common with mayo. You may trust it completely");
+} else {
+	alert("You're safe! It's ok! IT'S NOT MAYO.");
+}
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
