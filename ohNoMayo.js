6f623081e5a342bb0e363aa0328f87f634e0aa55
Tess Myers <tess@tessmyers.com>
2014-07-15 17:33:33 -0700

    I have no idea what this change was.

--- ohNoMayo.js
+++ ohNoMayo.js
@@ -4,15 +4,15 @@
 
 var condiment = prompt("Is it mayo? Enter questionable condiment to find out:");
 if (condiment === "mayo") {
-	alert("Oh fuck, it's mayo! RUN FOR IT.");
-} else if(condiment === "aioli") {
-	alert("Aioli is nectar of the gods and has absolutely nothing whatsoever in common with mayo. You may trust it completely.");
+  alert("Oh fuck, it's mayo! RUN FOR IT.");
+} else if (condiment === "aioli") {
+  alert("Aioli is nectar of the gods and has absolutely nothing whatsoever in common with mayo. You may trust it completely.");
 } else {
-	var text = condiment;
-	var target = "mayo";
-	var hits = [];
-	for(var i = 0; i < text.length; i++) {
-    if (text[i] === "m"){
+  var text = condiment;
+  var target = "mayo";
+  var hits = [];
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] === "m") {
       var preHits = [];
       for (var j = i; j < (target.length + i); j++) {
         preHits.push(text[j]);
@@ -21,12 +21,12 @@ if (condiment === "mayo") {
         hits.push(preHits.join(""));
       }
     }
-	}
-	if (hits.length === 0) {
-      alert("You're safe! It's ok! IT'S NOT MAYO.");
-	} else {
-      alert("Oh fuck, there's mayo somewhere in there! GET TO THE CHOPPAH.");
-	}
+  }
+  if (hits.length === 0) {
+    alert("You're safe! It's ok! IT'S NOT MAYO.");
+  } else {
+    alert("Oh fuck, there's mayo somewhere in there! GET TO THE CHOPPAH.");
+  }
 }
 
 
